54aa1e4f6e1820562bd84f086499002272bbcbb5
adeel <muhdkhokhar2@gmmail.com>
2014-11-22 17:41:28 +0000

    crudd anc cachde

--- spring-mybatis/src/main/java/makky/mybatis/tutorial/IUserMgmtDao.java
+++ spring-mybatis/src/main/java/makky/mybatis/tutorial/IUserMgmtDao.java
@@ -3,6 +3,7 @@ package makky.mybatis.tutorial;
 import java.util.List;
 
 import makky.mybatis.result.UserDetailDepartmentResult;
+import makky.mybatis.tutorial.domain.Department;
 
 import org.apache.ibatis.annotations.Param;
 
@@ -17,5 +18,11 @@ public interface IUserMgmtDao {
 	public List<UserDetailDepartmentResult> getUserDetail(
 			@Param("deptName") final String deptName,
 			@Param("username") final String username);
+	
+	public void addDepartment(final Department dept);
+	
+	public int deleteDepartmentByName(final String name);
+	
+	public int updateDepartmentNameById(@Param("name") String deptName , @Param("id") Integer id );
 
 }
--- spring-mybatis/src/test/java/tests/DaoTest.java
+++ spring-mybatis/src/test/java/tests/DaoTest.java
@@ -1,9 +1,11 @@
 package tests;
 
 import java.util.List;
+import java.util.UUID;
 
 import makky.mybatis.result.UserDetailDepartmentResult;
 import makky.mybatis.tutorial.IUserMgmtDao;
+import makky.mybatis.tutorial.domain.Department;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -42,6 +44,49 @@ public class DaoTest {
 		for (UserDetailDepartmentResult userDetailDepartmentResult : userDetail) {
 			System.out.println(userDetailDepartmentResult.getUsername());
 			System.out.println(userDetailDepartmentResult.getDate_added());
+		}
+
+	}
+
+	@Test
+	public void testCache() throws InterruptedException {
+		for (int i = 0; i < 100; i++) {
+			long start = System.currentTimeMillis();
+			Integer countForDepartment = mgmtDao.getCountForDepartment("%%");
+			System.out.println("\n Total count = " + countForDepartment);
+			long end = System.currentTimeMillis();
+			long totalMili = end - start;
+			System.out.println("Total time = " + totalMili);
+			Thread.sleep(2000);
+			if(i%10==0){
+				Department dept = new Department();
+				dept.setDepartment_name(UUID.randomUUID().toString());
+				mgmtDao.addDepartment(dept);
+				System.out.println("\n=======================\nadded new dept...");
+			}
+
+		}
+	}
+
+	@Test
+	public void testCRUD() {
+		// Department dept=new Department();
+		// dept.setDepartment_name(UUID.randomUUID().toString());
+		// mgmtDao.addDepartment(dept);
+		// System.out.println("The inserted id " + dept.getId());
+
+		// int deleteDepartmentByName =
+		// mgmtDao.deleteDepartmentByName("last department");
+		// System.out.println("total deleted = " + deleteDepartmentByName);
+
+		// int updateDepartmentNameById = mgmtDao.updateDepartmentNameById(
+		// "new department", 3);
+		// System.out.println("total updated = " + updateDepartmentNameById);
+		for (int i = 0; i < 10000; i++) {
+			Department dept = new Department();
+			dept.setDepartment_name(UUID.randomUUID().toString());
+			mgmtDao.addDepartment(dept);
+			System.out.println("total added = " + i);
 		}
 
 	}
